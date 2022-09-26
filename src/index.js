8634429d07e30ab8ae19c9437da39ae708887db7
andrunevychHalyna <andrunevychhalyna@gmail.com>
2022-09-26 17:53:53 -0600

    Add routing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+
 
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+      <Routes>
+          <Route path="/" element={<App/>}>
+          <Route path="/expenses" element={<Expenses/>}/>
+          <Route path="/invoices" element={<Invoices/>}/>
+          </Route>
+      </Routes>
   </BrowserRouter>,
   document.getElementById('root')
 );
